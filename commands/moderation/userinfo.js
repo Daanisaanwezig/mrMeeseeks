2b5bb3500d775abc819d5a43f661e34223203b41
Cory Metcalfe <corymetcalfe@gmail.com>
2020-10-25 22:33:35 -0500

    refactor to improve userinfo redundancy

--- commands/moderation/userinfo.js
+++ commands/moderation/userinfo.js
@@ -13,62 +13,48 @@ module.exports.run = async (bot, message, args) =>
 
     // If the member who ran the command is not a moderator the code gets canceled
     if(!message.member.roles.find(r => r.id === roles.moderator))
-	{ 
-		return;
+    { 
+        return;
     }
 
-    // If no user is given information will be given about the user who ran the command
+    var user;
+    var embed = new Discord.RichEmbed()
+        .setColor(colors.green)
+        .setTitle(`User info`)
+        .setFooter(`Mr. Meeseeks will now stop existing... **POOF**`);
+
+    
     if (!args[0])
     {
-        var user = bot.users.get(message.member.id);
-        var guser = message.guild.members.get(message.member.id);
-
-        const embed = new Discord.RichEmbed() //create a new embed builder
-            .setColor(colors.green)
-            .setTitle(`User info`)
-            .setDescription(`Here is some information I found about ${message.member}:`)
-            .addField("User id", `${message.member.id}`)
-            .addField('Joined server at:', `${moment.utc(guser.joinedAt).format('dddd, MMMM Do YYYY, HH:mm:ss')}`)
-            .addField("Account Created On:", `${moment.utc(user.createdAt).format("dddd, MMMM Do YYYY")}`, true) 
-            .addField('Status:', user.presence.status)
-            .addField('Roles:', guser.roles ? guser.roles.map(r => `${r}`).join(' | ') : "")
-            .setFooter(`Mr. Meeseeks will now stop existing... **POOF**`);
-        message.channel.send(embed);
+        // If no user is given information will be given about the user who ran the command
+        user = bot.users.get(message.member.id);
     }
-
-    // If a user is given it will use that
-    if (args[0])
-    {
-        var user = bot.users.find('username', args[0]);
+    else {
+        // If a user is given it will use that
+        user = bot.users.find('username', args[0]);
         if (!user)
         {
             user = message.mentions.users.first();
         }
-        if (user)
-        {
-            var guser = message.guild.members.get(user.id);
-    
-            const embed = new Discord.RichEmbed() //create a new embed builder
-                .setColor(colors.green)
-                .setTitle(`User info`)
-                .setDescription(`Here is some information I found about ${guser}:`)
-                .addField("User id", `${user.id}`)
-                .addField('Joined server at:', `${moment.utc(guser.joinedAt).format('dddd, MMMM Do YYYY, HH:mm:ss')}`)
-                .addField("Account Created On:", `${moment.utc(user.createdAt).format("dddd, MMMM Do YYYY")}`, true) 
-                .addField('Status:', user.presence.status)
-                .addField('Roles:', guser.roles ? guser.roles.map(r => `${r}`).join(' | ') : "")
-                .setFooter(`Mr. Meeseeks will now stop existing... **POOF**`);
-            message.channel.send(embed);
-        }
-        else
-        {
-            const embed = new Discord.RichEmbed() //create a new embed builder
-                .setColor(colors.red)
-                .setTitle(`User info`)
-                .setDescription(`User named "${args[0]}" was not found.`)
-                .setFooter(`Mr. Meeseeks will now stop existing... **POOF**`);
-            message.channel.send(embed);
-        }
+    }
+
+    if (!user)
+    {
+        embed.setColor(colors.red)
+            .setDescription(`User named "${args[0]}" was not found.`)
+        message.channel.send(embed);
+    }
+    else
+    {
+        var guser = message.guild.members.get(user.id);
+
+        embed.setDescription(`Here is some information I found about ${guser}:`)
+            .addField("User id", `${user.id}`)
+            .addField('Joined server at:', `${moment.utc(guser.joinedAt).format('dddd, MMMM Do YYYY, HH:mm:ss')}`)
+            .addField("Account Created On:", `${moment.utc(user.createdAt).format("dddd, MMMM Do YYYY")}`, true) 
+            .addField('Status:', user.presence.status)
+            .addField('Roles:', guser.roles ? guser.roles.map(r => `${r}`).join(' | ') : "")
+        message.channel.send(embed);
     }
 
 }
